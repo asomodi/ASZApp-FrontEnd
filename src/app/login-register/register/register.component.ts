ef1ee40c0dc48d4fbe2a9c3242dc456eafa0d668
Akos Somodi <akossomodi@yahoo.com>
2019-07-18 13:19:03 +0200

    append-register-component-to-previous-commit

--- src/app/login-register/register/register.component.ts
+++ src/app/login-register/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NewUser } from 'src/app/interfaces/new-user';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  isFirstPanelCompleted = false;
+  isSecondPanelCompleted = false;
+  isThirdPanelCompleted = false;
+
+  @Input()
+  newUser: NewUser;
+
+  constructor() {
+
+    this.newUser = {
+
+      userName: '',
+      email: '',
+      password1: '',
+      password2: ''
+    }
+
+  }
 
   ngOnInit() {
   }
